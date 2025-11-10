2971b2634e13abd3e69bf3e4d8e7c0e8222aa134
imane el arrach <imanearrache2@gmail.com>
2025-11-10 21:05:47 +0100

    ajout de TP JOB APPLICATION

---
+++ 4-TP-Jobs-app/job_application/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+export default { components: { Navbar } };
+</script>
+
+<style>
+body {
+  font-family: "Poppins", sans-serif;
+  margin: 0;
+  background-color: #f9f9f9;
+}
+</style>
